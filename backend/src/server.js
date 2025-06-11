a1cc30434f1ee0fc449f2e20ccbd796b57b88327
harpreet2004 <harpreetsingh5066@gmail.com>
2025-06-11 18:20:23 +0530

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import cors from "cors"
+import path from "path"
 
 import router from "./routes/notes.routes.js"
 import {connectDB} from "./config/db.js"
@@ -10,15 +11,31 @@ dotenv.config()
 
 const PORT = process.env.PORT || 5001;
 const app = express()
+const __dirname = path.resolve();
 
 
-app.use(cors({
-    origin: "http://localhost:5173"
-}))
+//middlewares
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors({
+        origin: "http://localhost:5173"
+    }))
+}
 app.use(express.json())
 app.use(rateLimiter);
 app.use("/api/notes", router)
 
+if(process.env.NODE_ENV == "production") {
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+    try {
+        app.get("/*", (req,res) => {
+        res.sendFile(path.join(__dirname,"../frontend","dist", "index.html"))
+    })
+    } catch (error) {
+        console.log("ERROR in server path.join");
+        console.log(error.message);  
+    }
+}
 
 connectDB().then(() => {
     app.listen(PORT, () => {
